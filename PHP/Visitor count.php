f7215de97d37a122afd0b8b40c3596c138d00afb
Daniel <github.com.rectify325@passmail.net>
2024-09-02 14:58:06 +0100

    Potentialy adding PHP

---
+++ PHP/Visitor count.php
@@ -0,0 +1,22 @@
+<?php
+// Path to the counter file
+$counter_file = 'counter.txt';
+
+// Initialize the counter file if it doesn't exist
+if (!file_exists($counter_file)) {
+    file_put_contents($counter_file, '0');
+}
+
+// Read the current count
+$count = (int)file_get_contents($counter_file);
+
+// Increment the count
+$count++;
+
+// Write the new count back to the file
+file_put_contents($counter_file, $count);
+
+// Return the count as a JSON response
+header('Content-Type: application/json');
+echo json_encode(['count' => $count]);
+?>
